c06c43b9aa7a84521e7ac7b34a9d254619d3fe7c
skc-garimella <skc.garimella@gmail.com>
2016-09-06 23:14:49 -0500

    commit

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -5,7 +5,7 @@ export default function (state = [], action){
 
   switch(action.type){
     case FETCH_WEATHER :
-      return state.concat([action.payload.data]);
+      return [action.payload.data].concat(state);
   };
 
   return state;
